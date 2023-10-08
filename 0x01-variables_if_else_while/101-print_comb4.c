bb9a56f1386539419db3c7a2c47cc098d4cf0697
Abigael Karushi <karushiabigael@gmail.com>
2023-10-08 13:04:59 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,7 +10,7 @@ for (digit1 = 0; digit1 <= 7; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 <= 8; digit2++)
 {
-for (digit3 = digit2 + 1; digit3 < 9; digit3++)
+for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
 {
 putchar(digit1 + '0');
 putchar(digit2 + '0');
