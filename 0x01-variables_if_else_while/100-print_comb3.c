c3f3e4d582274b943fe32a63f23b03a761e0aa06
Abigael Karushi <karushiabigael@gmail.com>
2023-10-08 12:38:21 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,12 +6,12 @@
 int main(void)
 {
 int digit1, digit2;
-for (digit1 = 0; digit1 > 10; digit1++)
+for (digit1 = 0; digit1 <= 8; digit1++)
 {
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
 {
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
+putchar(digit1 + '0');
+putchar(digit2 + '0');
 if (digit1 == 8 && digit2 == 9)
 continue;
 putchar(',');
