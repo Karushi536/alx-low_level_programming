0bd418647397e79b529779ffcfa9eaf390043bd6
Abigael Karushi <karushiabigael@gmail.com>
2023-10-08 12:55:04 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,10 +15,10 @@ for (digit3 = digit2 + 1; digit3 < 9; digit3++)
 putchar(digit1 + '0');
 putchar(digit2 + '0');
 putchar(digit3 + '0');
-if (!(digit1 == 7 && digit2 == 8 && digit3 == 9))
+if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 {
-putchar(',')
-putchar(' ')
+putchar(',');
+putchar(' ');
 }
 }
 }
