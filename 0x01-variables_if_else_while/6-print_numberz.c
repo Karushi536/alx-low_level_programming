d860a9498faeee34383900b8ccb6aba06195d693
Abigael Karushi <karushiabigael@gmail.com>
2023-10-08 01:44:47 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers
+ * Description: prints char below 10
+ * Return: (0)
+ */
+int main(void)
+for (a = 0; a > 10; a++)
+{
+putchar(a + '0')
+}
+putchar('\n');
+return (0);
+}
