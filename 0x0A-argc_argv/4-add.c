546ab2331c27639df3472d0b31badb5106f189e5
Abigael Karushi <karushiabigael@gmail.com>
2023-11-01 13:55:28 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,47 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <string.h>
 /**
  * main - program that adds positive numbers
- * argc: number of arguments
- * argv: array of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 success
  */
 int main(int argc, char *argv[])
-{
-	int sum = 0;
-	int i = 1;
+	{
+	int a;
+	unsigned int b, sum = 0;
+	char *c;
 
-	if (argc == 1)
+	if (argc > 1)
+	{
+		for (a = 1; a < argc; a++)
+		{
+			c = argv[a];
 
+			for (b = 0; b < strlen(c); b++)
+			{
+				if (c[b] < 48 || c[b] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(c);
+			c++;
+		}
+
+		printf("%d\n", sum);
+	}
+	else
 	{
 		printf("0\n");
-		return (0);
 	}
-	char *arg;
-	int num, j;
 
-	while (i < argc)
-	{
-		char *arg = argv[i];
-
-		num = 0;
-		j = 0;
-
-		while (arg[j] != '\0')
-		{
-			if (arg[j] < '0' || arg[j] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-			num = num * 10 + (arg[j] - '0');
-			j++;
-		}
-		sum += num;
-		i++;
-	}
-	printf("%d\n", sum);
 	return (0);
 }
