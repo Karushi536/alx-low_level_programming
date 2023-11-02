adc29a6361b340e359f41a29f7bfcde1b760de8b
Abigael Karushi <karushiabigael@gmail.com>
2023-11-02 06:53:16 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,17 +6,15 @@
  * @b: The size of the memory block to allocate.
  *
  * Return: A pointer to the allocated memory.
- * If malloc fails, the program is terminated with a status value of 98.
  */
 
 void *malloc_checked(unsigned int b)
 {
-void *ptr = malloc(b);
+	void *ptr = malloc(b);
 
 	if (ptr == NULL)
 	{
-	fprintf(stderr, "Memory allocation failed\n");
-	exit(98);
+		exit(98);
 	}
 
 	return (ptr);
