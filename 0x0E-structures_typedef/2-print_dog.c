4fc29161e2cb9913ceee170c311498eb2320b7fb
Abigael Karushi <karushiabigael@gmail.com>
2023-11-07 10:22:14 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,10 +17,9 @@ void print_dog(struct dog *d)
 
 		printf("Age: %f\n", d->age);
 
-		 if (d->owner == NULL)
-			 printf("Owner: (nil)\n");
-
-		 else
-			 printf("Owner: %s\n", d->owner);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 }
