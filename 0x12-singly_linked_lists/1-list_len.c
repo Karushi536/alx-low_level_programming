c0eefced925b21a47f56930b86d1651063116e7f
Abigael Karushi <karushiabigael@gmail.com>
2023-11-17 11:50:07 -0800

    Edited the 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -12,13 +12,13 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t node_count = 0;
+	size_t node_count = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-        node_count++;
-    }
+	while (h != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
 
-    return node_count;
+	return (node_count);
 }
