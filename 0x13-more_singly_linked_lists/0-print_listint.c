db681e4bf6f1c5aca8cc835e89cf410a481d8ed7
Abigael Karushi <karushiabigael@gmail.com>
2023-11-21 06:11:18 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,16 +7,16 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t node_position = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		node_position++;
 	}
 
-	return (count);
+	return (node_position);
 }
 
 
