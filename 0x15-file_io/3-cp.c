d6bb8a307f7a10238c90416738b08998a30d2376
Abigael Karushi <karushiabigael@gmail.com>
2023-11-27 15:21:16 -0800

    created a new file no 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,54 +14,49 @@
  */
 int main(int argc, char *argv[])
 {
-    int fd_from, fd_to, read_status, write_status;
-    char buffer[BUF_SIZE];
+	int fd_from, fd_to, read_status, write_status;
+	char buffer[BUF_SIZE];
 
-    // Check if the number of arguments is correct
-    if (argc != 3)
-    {
-        dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-        exit(97);
-    }
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
 
-    // Open the source file in read-only mode
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1)
-    {
-        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-    // Open the destination file in write-only mode, create if not exists, truncate
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd_to == -1)
-    {
-        dprintf(2, "Error: Can't write to %s\n", argv[2]);
-        close(fd_from);
-        exit(99);
-    }
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
+		exit(99);
+	}
 
-    // Read from the source file and write to the destination file
-    while ((read_status = read(fd_from, buffer, BUF_SIZE)) > 0)
-    {
-        write_status = write(fd_to, buffer, read_status);
-        if (write_status != read_status)
-        {
-            dprintf(2, "Error: Can't write to %s\n", argv[2]);
-            close(fd_from);
-            close(fd_to);
-            exit(99);
-        }
-    }
+	while ((read_status = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		write_status = write(fd_to, buffer, read_status);
+		if (write_status != read_status)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
 
-    // Check for read errors
-    if (read_status == -1)
-    {
-        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-        close(fd_from);
-        close(fd_to);
-        exit(98);
-    }
+	if (read_status == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
 
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
