ff57bdf39e27f7da88b4b515a3aae0b5fca2c63b
Abigael Karushi <karushiabigael@gmail.com>
2023-11-27 15:28:31 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,55 +14,43 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, read_status, write_status;
-	char buffer[BUF_SIZE];
+	int file_des1, file_des2, sto;
+	char bufa[1024];
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	file_des1 =  open(argv[1], O_RDONLY, 0);
+	if (file_des1 == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+	file_des2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_des2 == 1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-
-	while ((read_status = read(fd_from, buffer, BUF_SIZE)) > 0)
+	while ((sto = read(file_des1, bufa, 1024)) > 0)
 	{
-		write_status = write(fd_to, buffer, read_status);
-		if (write_status != read_status)
+		if (write(file_des2, bufa, sto) != sto)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
 	}
-
-	if (read_status == -1)
+	if (sto == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	if (close(fd_from) == -1 || close(fd_to) == -1)
+	if (close(file_des1) != 0)
 	{
-		dprintf(2, "Error: Can't close fd\n");
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_des1), exit(100);
 	}
-
+	if (close(file_des2) != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_des2), exit(100);
+	}
 	return (0);
 }
